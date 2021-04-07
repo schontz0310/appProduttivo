4ba084f7a4403dc97e73761a76c9cbbdf8f9771e
schontz0310 <schontz0310@gmail.com>
2021-04-07 00:59:29 -0300

    Added QRcode Scan

--- src/pages/QRCodeReader/index.tsx
+++ src/pages/QRCodeReader/index.tsx
@@ -1,19 +1,70 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import {BarCodeReadEvent, RNCamera} from 'react-native-camera';
+import BarcodeMask from 'react-native-barcode-mask';
 
-import {Container, Title} from './styles'
+import {UpperSection, LowerSection} from './styles'
+import { View, TextInput, Vibration } from 'react-native';
+import { Container } from '../Dashboard/styles';
+import { StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 
 
 const Dashboard: React.FC = () => {
 
-
+  const showData = useCallback((scanned: BarCodeReadEvent)=>{
+    Vibration.vibrate(300);
+    console.log(scanned.data);
+  },[])
   return(
     <>
-    <Container>
-        <Title>QR Code Reader</Title>
-    </Container>
+      <Container>
+        <RNCamera
+          onBarCodeRead={(data)=>showData(data)}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+
+        >
+          <BarcodeMask
+          width={300} height={300} showAnimatedLine={false} outerMaskOpacity={0.8}
+          />
+        </RNCamera>
+      </Container>
     </>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width:'120%'
+
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    alignSelf: 'center',
+  },
+});
+
 export default Dashboard;
+function data(data: any): ((event: import("react-native-camera").BarCodeReadEvent) => void) | undefined {
+  throw new Error('Function not implemented.');
+}
+
--- src/pages/QRCodeReader/styles.ts
+++ src/pages/QRCodeReader/styles.ts
@@ -3,9 +3,16 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
-  padding: 0 30px;
+  flex-direction: column;
+  background-color: #3b3b;
+`
+
+export const UpperSection = styled.View`
+  flex: 1;
+`;
+
+export const LowerSection = styled.View`
+  background-color: #fff;
 `;
 
 export const Title = styled.Text`
